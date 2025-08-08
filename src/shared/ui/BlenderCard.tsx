ad0ac411de68477cff2ee521e9e98f6c939bb8f9
Evgeniy Trubnikov <kiwibirdparmesan@gmail.com>
2025-08-08 16:17:12 +0300

    update BlenderCard

--- src/shared/ui/BlenderCard.tsx
+++ src/shared/ui/BlenderCard.tsx
@@ -1,6 +1,7 @@
 import type { ComponentProps, FC } from "react";
 import { Card } from "./Card";
 import DownarrowHitSvg from "@assets/svg/downarrow_hlt.svg?react";
+import GripSvg from "@assets/svg/grip.svg?react";
 import { cn } from "@shared/utils/cn";
 
 type BlenderCardProps = {
@@ -19,13 +20,16 @@ export const BlenderCard: FC<BlenderCardProps> = ({
       {...props}
       bgColor="foreground"
       className={cn(
-        "flex flex-col items-start justify-start gap-2",
+        "flex flex-col items-start justify-start gap-2 pt-0.5",
         cardClassName
       )}
     >
-      <div className="flex items-center">
-        <DownarrowHitSvg />
-        <h6>{title}</h6>
+      <div className="flex justify-between items-center w-full">
+        <div className="flex items-center">
+          <DownarrowHitSvg />
+          <h6>{title}</h6>
+        </div>
+        <GripSvg />
       </div>
       <div className={cn("", className)}>{children}</div>
     </Card>
